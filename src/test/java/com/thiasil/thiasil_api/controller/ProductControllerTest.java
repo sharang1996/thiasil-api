96ce16d078a31a45c5f643f862bdc9818ec3c249
sharang <sharanggupta96@gmail.com>
2024-08-11 13:33:20 +0100

    Adding a contract test skeleton

---
+++ src/test/java/com/thiasil/thiasil_api/controller/ProductControllerTest.java
@@ -0,0 +1,42 @@
+package com.thiasil.thiasil_api.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getProductsByCategory() {
+    }
+
+    @Test
+    void createProduct() {
+    }
+
+    @Test
+    void updateProduct() {
+    }
+
+    @Test
+    void deleteProduct() {
+    }
+
+    @Test
+    void createProducts() {
+    }
+
+    @Test
+    void getAllProducts() {
+    }
+}
